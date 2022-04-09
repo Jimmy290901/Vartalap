27417fb8dfa78f994e26842b792d52f9330c7b88
Jimmy290901 <shubham290901@gmail.com>
2022-04-09 16:03:44 +0530

    Created chat section header

--- whatsapp-mern/src/components/Chat/Chat.jsx
+++ whatsapp-mern/src/components/Chat/Chat.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Chat.css";
+import Avatar from '@mui/material/Avatar';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am the chat box</h1>
+            <div className="chat_header">
+                <Avatar />
+                <div className="chat_header_info">
+                    <h3>Room Name</h3>
+                    <p>Last seen: 12:43 P.M</p>
+                </div>
+                <div className="chat_header_options">
+                    <SearchIcon/>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>                
+                </div>
+            </div>
+            <div className="messages">
+                <p>Chat Messages</p>
+            </div>
         </div>
     );
 }
